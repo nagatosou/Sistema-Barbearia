01687ecb2fd1b6721b25dd1e7d6bbdfe8934f0bd
Daniel sousa reis <92447589+nagatosou@users.noreply.github.com>
2023-08-08 15:52:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ def main():
         print("4. Atualizar")
         print("0. Sair")
         
-        opcao = input("Escolha uma opção: ")  # Adicione esta linha para ler a opção
+        opcao = input("Escolha uma opção: ")
         
         if opcao == '1':
-            # ... Seu código para ler os dados do usuário ...
-
+            data = input("Digite a data de cadastro (DDMMAAAA): ")
+            cpf = input("Digite o CPF: ")
+            nome = input("Digite o nome: ")
+            endereco = input("Digite o endereço: ")
+            telefone = input("Digite o telefone: ")
+            
             codigo = controller.cadastrar(data, cpf, nome, endereco, telefone)
             if codigo:
                 print("Cadastro realizado com sucesso!")
@@ -23,8 +27,7 @@ def main():
                 print("Erro ao cadastrar.")
         
         elif opcao == '2':
-            # ... Seu código para ler o código de cadastro ...
-
+            codigo = input("Digite o código do cadastro: ")
             cadastro = controller.consultar(codigo)
             if cadastro:
                 print("Código:", cadastro[0])
@@ -35,24 +38,30 @@ def main():
                 print("Telefone:", cadastro[5])
             else:
                 print("Cadastro não encontrado.")
-
+                
         elif opcao == '3':
-            # ... Seu código para ler o código de cadastro ...
-
+            codigo = input("Digite o código do cadastro a ser deletado: ")
             if controller.deletar(codigo):
                 print("Cadastro deletado com sucesso.")
             else:
                 print("Cadastro não encontrado.")
-
+                
         elif opcao == '4':
-            # ... Seu código para ler os dados do usuário ...
-
+            codigo = input("Digite o código do cadastro a ser atualizado: ")
+            novo_data = input("Digite a nova data de cadastro (DD/MM/AAAA): ")
+            novo_cpf = input("Digite o novo CPF: ")
+            novo_nome = input("Digite o novo nome: ")
             if controller.atualizar(codigo, novo_data, novo_cpf, novo_nome):
                 print("Cadastro atualizado com sucesso.")
             else:
                 print("Cadastro não encontrado.")
-
-        # ... Resto do seu código ...
-
+                
+        elif opcao == '0':
+            print("Saindo do programa.")
+            break
+        
+        else:
+            print("Opção inválida. Tente novamente.")
+            
 if __name__ == "__main__":
     main()
