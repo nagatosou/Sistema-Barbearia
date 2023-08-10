99c4952175e2b6f8bd34338f2b6a7a4ac4c5beab
Daniel sousa reis <92447589+nagatosou@users.noreply.github.com>
2023-08-10 17:30:06 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,20 +4,21 @@ import random
 def gerar_codigo():
     return ''.join(random.choices('0123456789', k=3))
 
-def cadastrar(data, cpf, nome, endereco, telefone):
+def cadastrar(data, cpf, nome):
     codigo = gerar_codigo()
     with open('cadastros.csv', 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow([codigo, data, cpf, nome, endereco, telefone])
+        writer.writerow([codigo, data, cpf, nome])
     return codigo
 
-def consultar(codigo):
+def consultar_todos_ids_e_nomes():
+    id_nome_list = []
     with open('cadastros.csv', 'r', newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            if len(row) >= 1 and row[0] == codigo:
-                return row
-    return None
+            if len(row) >= 4:
+                id_nome_list.append((row[0], row[3]))  # Adiciona o ID e nome à lista de tuplas
+    return id_nome_list
 
 def deletar(codigo):
     registros = []
@@ -118,3 +119,16 @@ def atualizar_campo(self, codigo, campo, novo_valor):
                 writer.writerow(registro)
         return True
     return False
+
+def cadastrar_dados_adicionais(codigo, email, telefone, endereco):
+    with open('dados_adicionais.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([codigo, email, telefone, endereco])
+
+def consultar_dados_adicionais(codigo):
+    with open('dados_adicionais.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) >= 1 and row[0] == codigo:
+                return row[1], row[2], row[3]
+    return None, None, None
