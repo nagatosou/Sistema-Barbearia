81542fed0e102d1411dbc0dc92dfb005e440489e
Daniel sousa reis <92447589+nagatosou@users.noreply.github.com>
2023-08-11 17:14:07 -0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,134 +1,72 @@
 import csv
 import random
+from datetime import datetime, timedelta
 
-def gerar_codigo():
-    return ''.join(random.choices('0123456789', k=3))
+class Backend:
 
-def cadastrar(data, cpf, nome):
-    codigo = gerar_codigo()
-    with open('cadastros.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([codigo, data, cpf, nome])
-    return codigo
+    @staticmethod
+    def gerar_codigo():
+        return ''.join(random.choices('0123456789', k=3))
 
-def consultar_todos_ids_e_nomes():
-    id_nome_list = []
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if len(row) >= 4:
-                id_nome_list.append((row[0], row[3]))  # Adiciona o ID e nome à lista de tuplas
-    return id_nome_list
-
-def deletar(codigo):
-    registros = []
-    encontrado = False
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if len(row) >= 1 and row[0] == codigo:
-                encontrado = True
-            else:
-                registros.append(row)
-    
-    if encontrado:
-        with open('cadastros.csv', 'w', newline='') as csvfile:
+    def cadastrar(self, data, cpf, nome):
+        codigo = self.gerar_codigo()
+        with open('cadastros.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            for registro in registros:
-                writer.writerow(registro)
-        return True
-    else:
+            writer.writerow([codigo, data, cpf, nome])
+        return codigo
+
+    def consultar_todos_ids_e_nomes(self):
+        id_nome_list = []
+        with open('cadastros.csv', 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if len(row) >= 4:
+                    id_nome_list.append((row[0], row[3]))  # Adiciona o ID e nome à lista de tuplas
+        return id_nome_list
+
+    # Restante das funções...
+
+    def atualizar_campo(self, codigo, campo, novo_valor):
+        registros = []
+        encontrado = False
+        with open('cadastros.csv', 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if row[0] == codigo:
+                    row[self.campos_indices[campo]] = novo_valor
+                    registros.append(row)
+                    encontrado = True
+                else:
+                    registros.append(row)
+        if encontrado:
+            with open('cadastros.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                for registro in registros:
+                    writer.writerow(registro)
+            return True
         return False
 
-def atualizar_nome(codigo, novo_nome):
-    registros = []
-    encontrado = False
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if row[0] == codigo:
-                row[3] = novo_nome
-                registros.append(row)
-                encontrado = True
-            else:
-                registros.append(row)
-    if encontrado:
-        with open('cadastros.csv', 'w', newline='') as csvfile:
+    def cadastrar_dados_adicionais(self, codigo, email, telefone, endereco):
+        with open('dados_adicionais.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            for registro in registros:
-                writer.writerow(registro)
-        return True
-    return False
+            writer.writerow([codigo, email, telefone, endereco])
 
-def atualizar_cpf(codigo, novo_cpf):
-    registros = []
-    encontrado = False
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if row[0] == codigo:
-                row[2] = novo_cpf
-                registros.append(row)
-                encontrado = True
-            else:
-                registros.append(row)
-    if encontrado:
-        with open('cadastros.csv', 'w', newline='') as csvfile:
+    # Restante das funções...
+
+    def cadastrar_plano(self, codigo, duracao):
+        data_inicio = datetime.now().strftime('%d/%m/%Y')
+        data_fim = (datetime.strptime(data_inicio, '%d/%m/%Y') + timedelta(days=duracao)).strftime('%d/%m/%Y')
+        with open('planos.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            for registro in registros:
-                writer.writerow(registro)
-        return True
-    return False
+            writer.writerow([codigo, data_inicio, data_fim, duracao])
 
-def atualizar_telefone(codigo, novo_telefone):
-    registros = []
-    encontrado = False
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if row[0] == codigo:
-                row[4] = novo_telefone
-                registros.append(row)
-                encontrado = True
-            else:
-                registros.append(row)
-    if encontrado:
-        with open('cadastros.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            for registro in registros:
-                writer.writerow(registro)
-        return True
-    return False
-
-def atualizar_campo(self, codigo, campo, novo_valor):
-    registros = []
-    encontrado = False
-    with open('cadastros.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if row[0] == codigo:
-                row[self.campos_indices[campo]] = novo_valor
-                registros.append(row)
-                encontrado = True
-            else:
-                registros.append(row)
-    if encontrado:
-        with open('cadastros.csv', 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            for registro in registros:
-                writer.writerow(registro)
-        return True
-    return False
-
-def cadastrar_dados_adicionais(codigo, email, telefone, endereco):
-    with open('dados_adicionais.csv', 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([codigo, email, telefone, endereco])
-
-def consultar_dados_adicionais(codigo):
-    with open('dados_adicionais.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            if len(row) >= 1 and row[0] == codigo:
-                return row[1], row[2], row[3]
-    return None, None, None
+    def verificar_dias_plano(self, codigo):
+        data_atual = datetime.now()
+        with open('planos.csv', 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if len(row) >= 4 and row[0] == codigo:
+                    data_fim = datetime.strptime(row[2], '%d/%m/%Y')
+                    dias_restantes = (data_fim - data_atual).days
+                    return dias_restantes
+        return None
